6de331e87cf1d26ce03e034ccc1766548bf997f7
11aghayan <11.aghayan.99@gmail.com>
2024-09-06 23:46:26 +0400

    api url changed

--- src/util/axios.ts
+++ src/util/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Production URL
-const BASE_URL = 'https://api.garikaghayan.top/api/v1';
+const BASE_URL = 'https://www.api.garikaghayan.top/api/v1';
 
 // Test URL
 // const BASE_URL = 'http://localhost:3200/api/v1';
